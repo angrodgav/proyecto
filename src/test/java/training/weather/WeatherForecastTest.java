124929af34594e6452c60899e0fb6ca66a7489e2
angrodgav <kirchhoff84@gmail.com>
2021-08-30 04:00:17 +0200

    Test de funcionamiento inicial del metodo weatherForecast.getCityWeather(String city, Date datetime)

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -1,17 +1,96 @@
 package training.weather;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+
+import org.json.JSONException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WeatherForecastTest {
 
-	@Test
-	public void unfinished_test() throws IOException {
-		WeatherForecast weatherForecast = new WeatherForecast();
-		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
-		System.out.println(forecast);
+	private WeatherForecast weatherForecast;
+	
+	@Before
+	public void setUp() {
+		weatherForecast = new WeatherForecast();
 	}
+	
+	@Test
+	public void Give_CorrectParameters_WhenGetCityWeather_Then_GetCorrectWeatherStates() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
+		assertNotNull(forecast);
+	}
+	
+	@Test(expected = JSONException.class)
+	public void Give_NullCity_WhenGetCityWeather_Then_ThrowJSONException() throws IOException {
+		weatherForecast.getCityWeather(null, new Date());
+	}
+	
+	@Test
+	public void Give_NullDate_WhenGetCityWeather_Then_GetCorrectWeatherStates() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Madrid", null);
+		assertNotNull(forecast);
+	}
+	
+	@Test(expected = JSONException.class)
+	public void Give_WrongCity_WhenGetCityWeather_Then_ThrowJSONException() throws IOException {
+		weatherForecast.getCityWeather("Madrida", new Date());
+	}
+	
+	@Test
+	public void Give_PartOfCorrectCityName_WhenGetCityWeather_Then_GetCorrectWeatherStatesOfARamdomCityOfQuery() throws IOException {
+		String forecast = weatherForecast.getCityWeather("san", new Date());
+		assertNotNull(forecast);
+	}
+	
+	/**TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more  **/
+	@Test
+	public void Give_Plus6DaysLater_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
+		Date more144HoursLater = new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 6));
+		String forecast = weatherForecast.getCityWeather("Madrid",more144HoursLater );
+		assertEquals(forecast,"");
+	}
+	
+	/**TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more  **/
+	@Test
+	public void Give_Is6DaysLater_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
+		
+		final int numberHoursLess = 2;
+
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.HOUR_OF_DAY, 3);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		
+		Date less144HoursLaterButPlus6Days = new Date(c.getTime().getTime() + (1000 * 60 * 60 * (24 * 6 -numberHoursLess)));
+		String forecast = weatherForecast.getCityWeather("Madrid",less144HoursLaterButPlus6Days );
+		assertEquals(forecast,"");
+	}
+	
+	/**TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more  **/
+	@Test
+	public void Give_Is5DaysLater_WhenGetCityWeather_Then_GetCorrectWeatherStates() throws IOException {
+		
+		final int numberHoursLess = 4;
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.HOUR_OF_DAY, 3);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		
+		Date less144HoursLaterAndPlus5Days = new Date(c.getTime().getTime() + (1000 * 60 * 60 * (24 * 6 -numberHoursLess)));
+		String forecast = weatherForecast.getCityWeather("Madrid",less144HoursLaterAndPlus5Days );
+		assertNotNull(forecast);
+	}
+	
+	@Test
+	public void Give_PastDate_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
+		Date pastDate = new Date(new Date().getTime() - (1000 * 60 * 60 * 24 * 2));
+		String forecast = weatherForecast.getCityWeather("Madrid",pastDate );
+		assertEquals(forecast,"");
+	}	
 }
