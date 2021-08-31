21e7f7d02e08d6fe968351888cc4897c13de01ef
angrodgav <kirchhoff84@gmail.com>
2021-08-31 20:24:42 +0200

    refactor class WeatherForecastTest, delete unnecessary tests by the refactor of the validation from Date to LocalDate

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.json.JSONException;
@@ -12,16 +13,13 @@ import org.junit.Test;
 
 public class WeatherForecastTest {
 
-    private WeatherForecast weatherForecast = new WeatherForecast();
-
-    private static final int MILISECONDS_1_HOUR = 1000 * 60 * 60;
-    
-    private static final int HOURS_6_NORMAL_DAYS = 24 * 6;//sin cambio horario
-    
-    private static final int HOURS_2_NORMAL_DAYS = 24 * 2;//sin cambio horario
+    private static final LocalDate PREDICTION_LIMIT = LocalDate.now().plusDays(6);
+    private static final LocalDate LAST_DATE = LocalDate.now().minusDays(2);
 
     private static final String EMPTY_STRING = "";
-    
+
+    private WeatherForecast weatherForecast = new WeatherForecast();
+
     @Test
     public void Given_CorrectParameters_WhenGetCityWeather_Then_GetCorrectWeatherStates() throws IOException {
         String forecast = weatherForecast.getCityWeather("Madrid", new Date());
@@ -50,49 +48,15 @@ public class WeatherForecastTest {
         assertNotNull(forecast);
     }
 
-    //TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more
     @Test
     public void Given_Plus6DaysLater_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
-        Date more144HoursLater = new Date(new Date().getTime() + (MILISECONDS_1_HOUR * HOURS_6_NORMAL_DAYS));
-        String forecast = weatherForecast.getCityWeather("Madrid", more144HoursLater);
+        String forecast = weatherForecast.getCityWeather("Madrid", Date.from(PREDICTION_LIMIT.atStartOfDay(ZoneId.systemDefault()).toInstant()));
         assertEquals(forecast, EMPTY_STRING);
-    }
-
-    //TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more
-    @Test
-    public void Given_Is6DaysLater_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
-
-        final int numberHoursLess = 2;
-
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, 3);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-
-        Date less144HoursLaterButPlus6Days = new Date(c.getTime().getTime() + (MILISECONDS_1_HOUR * (HOURS_6_NORMAL_DAYS - numberHoursLess)));
-        String forecast = weatherForecast.getCityWeather("Madrid", less144HoursLaterButPlus6Days);
-        assertEquals(forecast, EMPTY_STRING);
-    }
-
-    //TODO 144 hours does not necessarily mean 6 days due to the time change, we must refine more
-    @Test
-    public void Given_Is5DaysLater_WhenGetCityWeather_Then_GetCorrectWeatherStates() throws IOException {
-
-        final int numberHoursLess = 4;
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, 3);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-
-        Date less144HoursLaterAndPlus5Days = new Date(c.getTime().getTime() + (MILISECONDS_1_HOUR * (HOURS_6_NORMAL_DAYS - numberHoursLess)));
-        String forecast = weatherForecast.getCityWeather("Madrid", less144HoursLaterAndPlus5Days);
-        assertNotNull(forecast);
     }
 
     @Test
     public void Given_PastDate_WhenGetCityWeather_Then_GetEmptyResponse() throws IOException {
-        Date pastDate = new Date(new Date().getTime() - (MILISECONDS_1_HOUR * HOURS_2_NORMAL_DAYS));
-        String forecast = weatherForecast.getCityWeather("Madrid", pastDate);
+        String forecast = weatherForecast.getCityWeather("Madrid", Date.from(LAST_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant()));
         assertEquals(forecast, EMPTY_STRING);
     }
 }
